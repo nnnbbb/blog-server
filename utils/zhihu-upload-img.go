5d5b9236000cc3f9323b4b16b086c0daa50d435d
dreamyxu0123 <dreamyxu0123@gmail.com>
2025-10-29 19:39:08 +0800

    feat: Update upload images to Zhihu

--- utils/zhihu-upload-img.go
+++ utils/zhihu-upload-img.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"mime/multipart"
@@ -34,33 +35,41 @@ func getHeaders() http.Header {
 }
 
 // 上传本地图片
-func UploadImageLocal(filePath string) error {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+type ZhihuUploadResponse struct {
+	Src            string `json:"src"`
+	AnimationCover string `json:"animation_cover_src"`
+	Hash           string `json:"hash"`
+	Watermark      string `json:"watermark"`
+	WatermarkSrc   string `json:"watermark_src"`
+	OriginalHash   string `json:"original_hash"`
+	OriginalSrc    string `json:"original_src"`
+	RawHeight      int    `json:"data-rawheight"`
+	RawWidth       int    `json:"data-rawwidth"`
+	WatermarkHash  string `json:"watermark_hash"`
+	Class          string `json:"class"`
+}
 
+// 核心上传函数，传入 Reader 和文件名
+func uploadImageFromReader(reader io.Reader, filename string) (string, error) {
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 
-	// 创建 form 文件字段
-	part, err := writer.CreateFormFile("picture", filepath.Base(filePath))
+	part, err := writer.CreateFormFile("picture", filename)
 	if err != nil {
-		return err
-	}
-	_, err = io.Copy(part, file)
-	if err != nil {
-		return err
+		return "", err
 	}
 
-	// 添加额外字段
+	_, err = io.Copy(part, reader)
+	if err != nil {
+		return "", err
+	}
+
 	_ = writer.WriteField("source", "answer")
 	writer.Close()
 
 	req, err := http.NewRequest("POST", "https://www.zhihu.com/api/v4/uploaded_images", body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	req.Header = getHeaders()
@@ -69,59 +78,48 @@ func UploadImageLocal(filePath string) error {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer resp.Body.Close()
 
-	respData, _ := io.ReadAll(resp.Body)
-	fmt.Println("上传本地图片:", resp.StatusCode, string(respData))
-	return nil
+	respData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("上传图片:", resp.StatusCode, string(respData))
+
+	var result ZhihuUploadResponse
+	if err := json.Unmarshal(respData, &result); err != nil {
+		return "", err
+	}
+
+	url := fmt.Sprintf("https://pic1.zhimg.com/80/%s_1440w.png", result.OriginalHash)
+	return url, nil
+}
+
+// 上传本地图片
+func UploadImageLocal(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	return uploadImageFromReader(file, filepath.Base(filePath))
 }
 
 // 上传网络图片
-func UploadImageFromURL(imgURL string) error {
+func UploadImageFromURL(imgURL string) (string, error) {
 	resp, err := http.Get(imgURL)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("下载图片失败: %d", resp.StatusCode)
+		return "", fmt.Errorf("下载图片失败: %d", resp.StatusCode)
 	}
 
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	// 模拟文件上传
-	part, err := writer.CreateFormFile("picture", "remote.jpg")
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(part, resp.Body)
-	if err != nil {
-		return err
-	}
-
-	_ = writer.WriteField("source", "answer")
-	writer.Close()
-
-	req, err := http.NewRequest("POST", "https://www.zhihu.com/api/v4/uploaded_images", body)
-	if err != nil {
-		return err
-	}
-
-	req.Header = getHeaders()
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-
-	client := &http.Client{}
-	resp2, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp2.Body.Close()
-
-	respData, _ := io.ReadAll(resp2.Body)
-	fmt.Println("上传网络图片:", resp2.StatusCode, string(respData))
-	return nil
+	return uploadImageFromReader(resp.Body, "remote.jpg")
 }
